4767fda30bceae96284b489d672c0149d99138a5
kyho <ho.kahyoong@gmail.com>
2025-04-04 20:13:57 +0800

    docker build update

--- vue-frontend/nuxt.config.ts
+++ vue-frontend/nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  vite: {
+    server: {
+      fs: {
+        allow: ['.'],
+      },
+    },
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     // '@primevue/nuxt-module',
